2fff70668ff476ab507cef21a2f1133f61b2ca20
Dame <gban1mj27@gmail.com>
2025-05-07 15:07:31 +0700

    changed html to react and added some states

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,112 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const groceryItems = [
+  {
+    id: 1,
+    name: 'Kopi Bubuk',
+    quantity: 5,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: 'Gula Pasir',
+    quantity: 2,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: 'Kopi Good Day',
+    quantity: 1,
+    checked: false,
+  },
+]
+
+export default function App() {
 
   return (
-    <>
+    <div className="app">
+      <Header />
+      <Form />
+      <GroceryList />
+      <Footer />
+    </div>
+  )
+}
+
+function Header () {
+  return <h1>Catatan Belanjaku 📝</h1>
+}
+
+function Form () {
+
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) { 
+    e.preventDefault()
+
+    if(!name) return
+
+    const newItem = { name: name, quantity: quantity, checked: false, id: Date.now}
+    
+    // alert(quantity)
+    console.log(newItem); 
+    setName('')
+    setQuantity(1)
+  }
+
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ))
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+          { quantityNum }
+        </select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <button>Tambah</button>
+    </form>
+  )
+}
+
+function GroceryList() {
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id}/>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
     </>
   )
 }
 
-export default App
+function Item({item}) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={ item.checked ? { textDecoration: 'line-through' } : {} }>{item.quantity} {item.name}</span>
+      <button>&times;</button>
+    </li>
+  )
+}
+
+function Footer() {
+  return <footer className="stats">Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)</footer>
+}
